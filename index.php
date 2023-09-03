2ab0a8464706176efcafe9eafd05c4321248977c
danukaj <damayanthiamarakoon753@gmail.com>
2023-09-04 00:37:16 +0530

    2nd commit

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="h-100 background">
+    <div class="  container-fluid">
+        <div class="row">
+            <div class=" col-12 d-none d-lg-block align-items-center bg-info text-center">
+                <div class="mt-2 mb-2 ">
+                    <a href="#" class=" text-decoration-none">
+                        <span class=" mx-5 text-uppercase font-monospace me-5  h5 fw-bold text-black text-body "> HOMe </span>
+                    </a>
+                    <a href="#" class=" text-decoration-none">
+                        <span class=" mx-5 text-uppercase font-monospace me-5 h5 fw-bold text-black text-body "> Places of sri lanka </span>
+                    </a>
+                    <a href="#" class=" text-decoration-none">
+                        <span class=" mx-5 text-uppercase font-monospace me-5 h5 fw-bold text-black text-body "> hotels </span>
+                    </a>
+                    <a href="#" class=" text-decoration-none">
+                        <span class=" mx-5 text-uppercase font-monospace me-5 h5 fw-bold text-black text-body "> ancient places </span>
+                    </a>
+                    <a href="#" class=" text-decoration-none">
+                        <span class=" mx-5 text-uppercase font-monospace h5 me-5 fw-bold text-black text-body "> About us </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-12 text-center ">
+                <span class=" text-capitalize ">
+                    <p class=" text1">sri lanka <span class="text-warning text-decoration-underline ">this is your travel place</span></p>
+                </span>
+            </div>
+        </div>
+        <hr>
+        <div class="row ">
+            <div id="carouselExample" class="carousel slide align-items-center carousel-fade justify-content-center " data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="2500">
+                        <img src="resources/2.jpg" class="d-block w-100 " alt="...">
+                        <div class=" carousel-caption d-sm-none d-lg-block">
+                            <h1 class=" text-uppercase text-success-emphasis font-monospace ">sigiriya</h1>
+                            <p class=" font-monospace text-capitalize text-decoration-underline h3">sigiriya most important place in sri lanka </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2400">
+                        <img src="resources/1.jpg" class="d-block w-100" alt="...">
+                        <div class=" carousel-caption d-sm-none d-lg-block">
+                            <h1 class=" text-uppercase text-success-emphasis font-monospace ">nine arch bridge</h1>
+                            <p class=" font-monospace text-capitalize text-decoration-underline h3">oldest bridge in sri lanka </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2400">
+                        <img src="resources/3.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2400">
+                        <img src="resources/4.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2400">
+                        <img src="resources/5.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2400">
+                        <img src="resources/6.jpg" class="d-block w-100 " alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-12 text-center ">
+                <span class="h2 font-monospace text-black-50 text-uppercase ">most beautiful places in sri lanka</span>
+            </div>
+            <hr>
+            <div class="col-12 col-lg-6 ">
+                <span class="h3 fw-bold text-body-emphasis text-capitalize ">sri lanka </span>
+                <p class=" ">
+                    <b>Sri Lanka: A Land of Enchanting Beauty </b> <br />
+
+                    Nestled like a precious jewel in the heart of the Indian Ocean, Sri Lanka is a country that captivates the senses and leaves an indelible mark on the soul. Renowned for its breathtaking natural landscapes, rich cultural heritage, and warm hospitality, Sri Lanka is often referred to as the "Pearl of the Indian Ocean" for good reason. This island nation, with its diverse geography and vibrant culture, offers a tapestry of experiences that make it truly beautiful in every sense.
+
+
+                </p>
+                <p>
+                    One of Sri Lanka's most striking features is its stunning coastline. Miles of pristine, golden-sand beaches fringe the island, lapped by the crystal-clear waters of the Indian Ocean. Whether you're seeking solitude in a secluded cove or vibrant beach towns buzzing with activity, Sri Lanka has it all. Mirissa, Unawatuna, and Tangalle are just a few of the coastal gems that offer not only sun and surf but also a chance to witness magnificent marine life, including the majestic blue whales that frequent these waters.
+                </p>
+                <p>
+                    For nature enthusiasts, Sri Lanka is a paradise. The country is blessed with lush forests, verdant hills, and abundant wildlife. The central highlands are home to the picturesque city of Kandy, nestled amidst misty mountains and serene lakes. Here, you can visit the revered Temple of the Tooth, a UNESCO World Heritage Site, and explore the Royal Botanical Gardens, a horticultural wonderland.
+
+                </p>
+                <p>
+                    Venture further into the heart of the island, and you'll discover the enchanting landscapes of Ella. The Ella Gap offers panoramic views of rolling tea plantations, cascading waterfalls, and the iconic Ella Rock. A hike up Ella Rock at sunrise is a surreal experience, rewarded with a breathtaking vista that seems straight out of a postcard.
+                </p>
+                <p>
+                    Speaking of tea plantations, Sri Lanka is world-famous for its Ceylon tea. The emerald-green tea fields of Nuwara Eliya and the charming town of Haputale provide an opportunity to learn about the tea-making process while enjoying the crisp mountain air. Sip a cup of freshly brewed Ceylon tea amidst the lush plantations, and you'll understand why it's renowned worldwide.
+
+                    Sri Lanka's cultural richness is equally captivating. The ancient city of Anuradhapura, with its towering dagobas and sacred Bodhi tree, speaks to the country's deep-rooted Buddhist heritage. Polonnaruwa, another UNESCO World Heritage Site, boasts intricately carved temples and colossal stone sculptures that transport visitors back in time.
+
+                    The architectural marvel of Sigiriya, often referred to as the "Lion Rock," stands as a testament to Sri Lanka's artistic ingenuity. Ascending this massive rock fortress is a journey through history, with frescoes, gardens, and ancient ruins providing glimpses into a bygone era.
+                </p>
+
+                <p>
+                    Food lovers will delight in Sri Lanka's vibrant cuisine, where spices reign supreme. From fiery curries to fragrant rice dishes, the country's culinary offerings are a fusion of flavors influenced by its history and diverse communities. Don't miss the chance to savor a traditional Sri Lankan meal, served on a banana leaf for an authentic experience.
+
+                    Sri Lanka's beauty extends beyond its landscapes and history to its warm and welcoming people. The island's residents are known for their hospitality, making visitors feel like treasured guests. Engaging with locals and immersing oneself in their customs and traditions is an essential part of the Sri Lankan experience.
+
+                    In conclusion, Sri Lanka's beauty is a harmonious blend of natural wonders, cultural treasures, and the kindness of its people. This small yet diverse island nation offers an incredible range of experiences that leave an indelible mark on the soul of every traveler. Whether you seek adventure, relaxation, or cultural exploration, Sri Lanka is a land of enchantment that beckons you to discover its myriad charms. It's a place where beauty truly knows no bounds.
+                </p>
+            </div>
+            <div class="col-6 d-sm-none d-lg-block">
+                <div id="modal" class="modal">
+                    <div class="modal-container">
+                        <img id="current" src="" alt="">
+                    </div>
+                </div>
+                <div class="imgs">
+                    <div class="img-container">
+                        <img src="resources/lotus_tower.jpg" alt="">
+                        <div class="photo-overlay">
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/elephant.jpg" alt="">
+                        <div class="photo-overlay">
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/train.jpg" alt="">
+                        <div class="photo-overlay">
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/beach.png" alt="">
+                        <div class="photo-overlay">
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/para.jpg" alt="">
+                        <div class="photo-overlay">
+
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/squirrel.jpg" alt="">
+                        <div class="photo-overlay">
+
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/temple.jpg" alt="">
+                        <div class="photo-overlay">
+
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/rain2.jpg" alt="">
+                        <div class="photo-overlay">
+
+                        </div>
+                    </div>
+                    <div class="img-container">
+                        <img src="resources/sunrice.jpg" alt="">
+                        <div class="photo-overlay">
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-12 text-center bg-dark align align-items-baseline ">
+                <div class="row mt-5 ">
+                    <div class="col-4 col-lg-3 text-start ">
+                        <span class="text-warning text-capitalize  ">sri lanka </span>
+                        <span class="text-capitalize text-light ">is the most beautifull country in this world </span>
+                    </div>
+                    <div class="col-4 col-lg-3 text-start ">
+                        <span class="text-info font-monospace fw-bold  ">Contact</span>
+                        <br>
+                        <span class="text-light text-capitalize ">we are the sri lanka best web developers</span>
+                        <br>
+                        <span class=" text-warning text-uppercase " >aventura group of company&trade;</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="bootstrap.bundle.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
